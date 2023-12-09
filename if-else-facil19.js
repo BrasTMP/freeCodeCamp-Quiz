189b2307984cf57b2ba48c3c8270e64511904331
Andryel <121632051+BrasTMP@users.noreply.github.com>
2023-12-09 19:16:38 -0300

    Update if-else-facil19.js

---
+++ if-else-facil19.js
@@ -0,0 +1,15 @@
+const num1 = 2;
+const num2 = 3;
+const oper = "-";
+
+if (oper === "-") {
+    console.log(num1 - num2);
+} else if (oper === "+") {
+    console.log(num1 + num2);
+} else if (oper === "/") {
+    console.log(num1 / num2);
+} else if (oper === "*") {
+    console.log(num1 * num2);
+} else {
+    console.log("Insira uma operação válida!")
+}
